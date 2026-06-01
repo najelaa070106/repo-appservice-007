dbc3a3a3a70e5f71462952b788da1003defaba6c
najelaa070106 <najlaaprmt@gmail.com>
2026-06-01 13:32:59 +0700

    Add endpoint to get server time in Jakarta timezone

--- server.js
+++ server.js
@@ -9,5 +9,12 @@ app.get('/status', (req, res) => {
  
 app.get('/profil', (req, res) => { 
   res.json({ mataKuliah: 'Komputasi Awan', topik: 'Serverless Azure' }); }); 
+
+app.get('/waktu', (req, res) => { 
+  res.json({ 
+    waktuServer: new Date().toLocaleString('id-ID', { timeZone: 'Asia/Jakarta' }),
+    timestamp: new Date().toISOString()
+  }); 
+}); 
  
 app.listen(port, () => console.log(`Server berjalan pada port ${port}`)); 
